17d981f315bda9a5250535e8c1660d87ff812e74
samzah <szahir@hotmail.com>
2016-03-24 15:49:35 -0400

    groceries

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var $input = $('#todo-input');
+var $list = $(".list");
+
+$('form').on('submit', function (e) {
+  var $li = $('<li>');
+
+  e.preventDefault();
+  $li.html($input.val());
+  $input.val('');
+  $list.append($li);
+
+});
+
+$('ul').on('click', 'li', function (e) {
+	$(this).toggleClass('line');
+});
